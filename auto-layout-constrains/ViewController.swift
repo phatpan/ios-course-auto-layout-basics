83cbfa449ed37cff3c8e4ec9ac0c3fed5a2f4d85
phatcharaphan <phat.pan@gmail.com>
2016-09-01 15:56:45 +0700

    Initial Commit

---
+++ auto-layout-constrains/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  auto-layout-constrains
+//
+//  Created by phatcharaphan ananpreechakun on 9/1/2559 BE.
+//  Copyright © 2559 phatcharaphan ananpreechakun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
